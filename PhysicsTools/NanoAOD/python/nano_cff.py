ba06b7a51394a73126dd6a229a11bd1b9da5eefd
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2023-05-03 13:38:19 +0200

    Fix missing process argument

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -157,11 +157,11 @@ def nanoAOD_puppiRecluster(process, useExistingWeights=False, runOnMC=True):
     process = puppiJetMETReclusterFromMiniAOD(process, useExistingWeights, runOnMC)
     return process
 
-def nanoPuppiReclusterCustomize_MC():
+def nanoPuppiReclusterCustomize_MC(process):
     process = nanoAOD_puppiRecluster(process, runOnMC=True)
     return process
 
-def nanoPuppiReclusterCustomize_Data():
+def nanoPuppiReclusterCustomize_Data(process):
     process = nanoAOD_puppiRecluster(process, runOnMC=False)
     return process
 
