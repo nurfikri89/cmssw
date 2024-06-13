43774dd1ce23e11fb2a9cd16bbad33d357a437ba
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2024-06-13 09:39:38 +0200

    First working version to extract Pileup GenJets

---
+++ SimGeneral/PileupInformation/plugins/GenPUJetExtractor.cc
@@ -0,0 +1,239 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <unordered_map>
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Run.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/stream/EDProducer.h"
+#include "FWCore/Framework/interface/InputSourceDescription.h"
+#include "FWCore/Framework/interface/ModuleContextSentry.h"
+#include "FWCore/ServiceRegistry/interface/InternalContext.h"
+#include "FWCore/ServiceRegistry/interface/ModuleCallingContext.h"
+#include "FWCore/ServiceRegistry/interface/ParentContext.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+#include "FWCore/Sources/interface/VectorInputSourceFactory.h"
+#include "FWCore/Sources/interface/VectorInputSource.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Utilities/interface/ESGetToken.h"
+
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/Common/interface/ValueMap.h"
+#include "DataFormats/JetReco/interface/GenJetCollection.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+#include "Mixing/Base/interface/PileUp.h"
+#include "SimGeneral/MixingModule/interface/PileUpEventPrincipal.h"
+#include "SimDataFormats/CrossingFrame/interface/CrossingFramePlaybackInfoNew.h"
+
+using namespace edm;
+using namespace reco;
+using namespace std;
+
+namespace edm {
+  class ParameterSet;
+}
+
+class GenPUJetExtractor : public edm::stream::EDProducer<> {
+public:
+  GenPUJetExtractor(const edm::ParameterSet&);
+  ~GenPUJetExtractor() override;
+
+  // void produce(edm::StreamID, edm::Event&, const edm::EventSetup&) const override;
+  void produce(edm::Event&, const edm::EventSetup&);
+
+  bool getGenJets(EventPrincipal const& ep,
+                     ModuleCallingContext const*,
+                     int bcr,
+                     int id,
+                     int& offset,
+                     const edm::EventSetup& setup,
+                     edm::StreamID const&);
+
+  std::unique_ptr<GenJetCollection> outGenJetsPUAll_;
+  std::vector<int> outGenJetsPUAllIdx_;
+
+  std::vector<std::unique_ptr<GenJetCollection>> vecOutGenJets_;
+  std::vector<std::unique_ptr<math::XYZPointF>>  vecOutXYZ0_;
+  std::vector<std::unique_ptr<float>>            vecOutt0_;
+
+private:
+  // ----------member data ---------------------------
+  int minBunch_;
+  int maxBunch_;
+  int nMaxPUEvent_;
+  int puEvtCounter_;
+
+  edm::InputTag label_;
+  edm::InputTag inputTagPlayback_;
+
+  std::unique_ptr<PileUp> input_;
+  InputTag tag_;
+  InputTag tag_xyz0_;
+  InputTag tag_t0_;
+};
+
+
+GenPUJetExtractor::GenPUJetExtractor(const ParameterSet& cfg): 
+  minBunch_(0),maxBunch_(0),nMaxPUEvent_(100),
+  tag_(InputTag("ak4GenJetsNoNu")),
+  tag_xyz0_(InputTag("genParticles","xyz0")),
+  tag_t0_(InputTag("genParticles","t0"))
+{
+  //
+  //
+  //
+  inputTagPlayback_ = InputTag("mix", "", edm::InputTag::kSkipCurrentProcess);
+  consumes<CrossingFramePlaybackInfoNew>(inputTagPlayback_);
+  //
+  //
+  //
+  int averageNumber = 1;
+  std::unique_ptr<TH1F> h(new TH1F("h", "h", 10, 0, 10));
+  bool playback = true;
+  auto conf = std::make_shared<PileUpConfig>("input", averageNumber, h, playback); // practically a dummy PileUpConfig
+  input_ = std::make_unique<edm::PileUp>(cfg.getParameter<edm::ParameterSet>("input"), conf, consumesCollector(), false /*mixingConfigFromDB*/);
+
+  //
+  //
+  //
+  for(int iPU=0; iPU < nMaxPUEvent_; iPU++){
+    produces<GenJetCollection>("ak4GenJetsNoNuPUEvent"+std::to_string(iPU));
+    produces<math::XYZPointF>("xyz0PUEvent"+std::to_string(iPU));
+    produces<float>("t0PUEvent"+std::to_string(iPU));
+  }
+
+  produces<GenJetCollection>("ak4GenJetsNoNuFromPU");
+  produces<edm::ValueMap<int>>("ak4GenJetsNoNuFromPUEventIdx");
+}
+
+GenPUJetExtractor::~GenPUJetExtractor() {}
+
+void GenPUJetExtractor::produce(Event& iEvent, const EventSetup& iSetup) {
+  using namespace std::placeholders;
+
+  //
+  //
+  //
+  edm::Handle<CrossingFramePlaybackInfoNew> playbackInfo;
+  bool got = iEvent.getByLabel(inputTagPlayback_, playbackInfo);
+
+  //
+  //
+  //
+  ModuleCallingContext const* mcc = iEvent.moduleCallingContext();
+  std::vector<edm::SecondaryEventIDAndFileInfo> recordEventID;
+
+  //
+  //
+  //
+  int bunchIdx = 0;
+  size_t numberOfEvents = playbackInfo->getNumberOfEvents(bunchIdx, 0);
+
+  //
+  //
+  //
+  int playbackCounter = 0;
+  std::vector<SecondaryEventIDAndFileInfo>::const_iterator begin = playbackInfo->getEventId(playbackCounter);
+  playbackCounter += numberOfEvents;
+  std::vector<SecondaryEventIDAndFileInfo>::const_iterator end = playbackInfo->getEventId(playbackCounter);
+
+  //
+  //
+  //
+  vecOutGenJets_.clear();
+  vecOutXYZ0_.clear();
+  vecOutt0_.clear();
+  for(int iPU = 0; iPU < nMaxPUEvent_; iPU++){
+    vecOutGenJets_.emplace_back(std::make_unique<reco::GenJetCollection>());
+    vecOutXYZ0_.emplace_back(std::make_unique<math::XYZPointF>());
+    vecOutt0_.emplace_back(std::make_unique<float>());
+  }
+
+  outGenJetsPUAll_ = std::make_unique<reco::GenJetCollection>();
+  outGenJetsPUAllIdx_.clear();
+
+  //
+  //
+  //
+  int vertexOffset = 0;
+  puEvtCounter_ = 0;
+  input_->playPileUp(begin, end, recordEventID,std::bind(
+      &GenPUJetExtractor::getGenJets, this, _1, mcc, bunchIdx, _2, vertexOffset, std::ref(iSetup), iEvent.streamID()
+    )
+  );
+
+
+  for(int iPU = 0; iPU < nMaxPUEvent_; iPU++){
+    // std::cout << "ak4GenJetsNoNuPUEvent"+std::to_string(iPU) << std::endl;
+    iEvent.put(std::move(vecOutGenJets_[iPU]),"ak4GenJetsNoNuPUEvent"+std::to_string(iPU));
+    iEvent.put(std::move(vecOutXYZ0_[iPU]),   "xyz0PUEvent"+std::to_string(iPU));
+    iEvent.put(std::move(vecOutt0_[iPU]),     "t0PUEvent"+std::to_string(iPU));
+  }
+
+
+  edm::OrphanHandle<reco::GenJetCollection> orphanHandle = iEvent.put(std::move(outGenJetsPUAll_),"ak4GenJetsNoNuFromPU");
+  auto genJetPUAllIdxV = std::make_unique<edm::ValueMap<int>>();
+  edm::ValueMap<int>::Filler filler_genJetPUAllIdx(*genJetPUAllIdxV);
+  filler_genJetPUAllIdx.insert(orphanHandle, outGenJetsPUAllIdx_.begin(), outGenJetsPUAllIdx_.end());
+  filler_genJetPUAllIdx.fill();
+  iEvent.put(std::move(genJetPUAllIdxV),"ak4GenJetsNoNuFromPUEventIdx");
+}
+
+bool GenPUJetExtractor::getGenJets(EventPrincipal const& eventPrincipal,
+                                  ModuleCallingContext const* mcc,
+                                  int bunchCrossing,
+                                  int eventId,
+                                  int& vertexOffset,
+                                  const edm::EventSetup& setup,
+                                  StreamID const& streamID) {
+  InternalContext internalContext(eventPrincipal.id(), mcc);
+  ParentContext parentContext(&internalContext);
+  ModuleCallingContext moduleCallingContext(&moduleDescription());
+  ModuleContextSentry moduleContextSentry(&moduleCallingContext, parentContext);
+
+  if (puEvtCounter_ >= nMaxPUEvent_){
+    puEvtCounter_++;
+    return true;
+  }
+
+  std::shared_ptr<Wrapper<std::vector<reco::GenJet> > const> shPtr = getProductByTag<std::vector<reco::GenJet> >(eventPrincipal, tag_, &moduleCallingContext);
+
+  if (shPtr) {
+    auto prodColl = shPtr->product();
+    for (size_t i = 0; i < prodColl->size(); i++){
+      vecOutGenJets_[puEvtCounter_]->emplace_back(prodColl->at(i));
+      outGenJetsPUAll_->emplace_back(prodColl->at(i));
+      outGenJetsPUAllIdx_.emplace_back(puEvtCounter_);
+    }
+  }else{
+    std::cout << "Warning" << std::endl;
+  }
+
+  std::shared_ptr<Wrapper<math::XYZPointF> const> shPtr_xyz0 = getProductByTag<math::XYZPointF>(eventPrincipal, tag_xyz0_, &moduleCallingContext);
+  if (shPtr_xyz0) {
+    vecOutXYZ0_[puEvtCounter_].reset(new math::XYZPointF(*(shPtr_xyz0->product())));
+  }else{
+    std::cout << "Warning: no shPtr_xyz0" << std::endl;
+  }
+
+  std::shared_ptr<Wrapper<float> const> shPtr_t0 = getProductByTag<float>(eventPrincipal, tag_t0_, &moduleCallingContext);
+  if (shPtr_xyz0) {
+    vecOutt0_[puEvtCounter_].reset(new float(*(shPtr_t0->product())));
+  }else{
+    std::cout << "Warning: no shPtr_t0" << std::endl;
+  }
+  // std::cout << (*(shPtr_t0->product())) << std::endl;
+
+  puEvtCounter_++;
+  return true;
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_FWK_MODULE(GenPUJetExtractor);
---
+++ SimGeneral/PileupInformation/test/test_playback_raw_sim_cfg.py
@@ -0,0 +1,122 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("PLAYBACK")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(5),
+    # input = cms.untracked.int32(-1),
+)
+
+process.source = cms.Source("PoolSource",
+  # fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/n/nbinnorj/TestMixing/samples/Run3Winter24Digi/QCD_PT-15to7000_TuneCP5_Flat2022_13p6TeV_pythia8/GEN-SIM-RAW/133X_mcRun3_2024_realistic_v10-v2/2550001/44425c49-648b-42b9-a117-45ee3a4b6735.root')
+  fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/n/nbinnorj/TestMixing/samples/Run3Winter24Digi/DYto2L-4Jets_MLL-50_TuneCP5_13p6TeV_madgraphMLM-pythia8/GEN-SIM-RAW/133X_mcRun3_2024_realistic_v10_ext2-v2/2520010/9b0e6012-35c5-4500-b6e7-ecc23d5b3dd5.root')
+
+)
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+  playBack = cms.PSet(
+    engineName = cms.untracked.string('MixMaxRng'),
+    initialSeed = cms.untracked.uint32(12345)
+  ),
+)
+
+from test_playback_helpers import fileNamesMinBias
+# process.playBack = cms.EDAnalyzer("TestPlayback",
+#   input = cms.SecSource("EmbeddedRootSource",
+#     type = cms.string('fixed'),
+#     nbPileupEvents = cms.PSet(
+#       averageNumber = cms.double(1.0)
+#     ),
+#     fileNames = fileNamesMinBias
+#   )
+# )
+# process.p = cms.Path(process.playBack)
+
+process.GenPUJetExtractor = cms.EDProducer("GenPUJetExtractor",
+  input = cms.SecSource("EmbeddedRootSource",
+    type = cms.string('fixed'),
+    nbPileupEvents = cms.PSet(
+      averageNumber = cms.double(1.0)
+    ),
+    fileNames = fileNamesMinBias
+  )
+)
+# process.p = cms.Path(process.GenPUJetExtractor)
+
+
+
+from PhysicsTools.NanoAOD.common_cff import *
+from PhysicsTools.NanoAOD.nanogen_cff import nanoMetadata as _nanoMetadata
+from PhysicsTools.NanoAOD.jetMC_cff import genJetTable as _genJetTable
+from PhysicsTools.NanoAOD.genVertex_cff import genVertexTable as _genVertexTable
+from PhysicsTools.NanoAOD.genVertex_cff import genVertexT0Table as _genVertexT0Table
+
+process.nanoMetadata = _nanoMetadata.clone()
+
+_genJetTableTemp = _genJetTable.clone()
+_genJetTableTemp.variables.chargedHadronEnergy = Var("chargedHadronEnergy()", float,doc="chargedHadronEnergy()",precision=12)
+_genJetTableTemp.variables.neutralHadronEnergy = Var("neutralHadronEnergy()", float,doc="neutralHadronEnergy()",precision=12)
+_genJetTableTemp.variables.chargedEmEnergy     = Var("chargedEmEnergy()", float,doc="chargedEmEnergy()",precision=12)
+_genJetTableTemp.variables.neutralEmEnergy     = Var("neutralEmEnergy()", float,doc="neutralEmEnergy()",precision=12)
+_genJetTableTemp.variables.muonEnergy          = Var("muonEnergy()", float,doc="muonEnergy()",precision=12)
+_genJetTableTemp.variables.chargedHadronMultiplicity = Var("chargedHadronMultiplicity()", "uint8", doc="chargedHadronMultiplicity()")
+_genJetTableTemp.variables.neutralHadronMultiplicity = Var("neutralHadronMultiplicity()", "uint8", doc="neutralHadronMultiplicity()")
+_genJetTableTemp.variables.chargedEmMultiplicity = Var("chargedEmMultiplicity()", "uint8", doc="chargedEmMultiplicity()")
+_genJetTableTemp.variables.neutralEmMultiplicity = Var("neutralEmMultiplicity()", "uint8", doc="neutralEmMultiplicity()")
+_genJetTableTemp.variables.muonMultiplicity = Var("muonMultiplicity()", "uint8", doc="muonMultiplicity()")
+
+nMaxPUEvent=100
+for iPU in range(0,nMaxPUEvent):
+  setattr(process,f"genJet{iPU}Table", _genJetTableTemp.clone())
+  getattr(process,f"genJet{iPU}Table").name=f"PUEvt{iPU}GenJet"
+  getattr(process,f"genJet{iPU}Table").src= cms.InputTag("GenPUJetExtractor",f"ak4GenJetsNoNuPUEvent{iPU}")
+  getattr(process,f"genJet{iPU}Table").cut=""
+  setattr(process,f"genVertex{iPU}Table", _genVertexTable.clone())
+  getattr(process,f"genVertex{iPU}Table").name=f"PUEvt{iPU}GenVtx"
+  getattr(process,f"genVertex{iPU}Table").src= cms.InputTag("GenPUJetExtractor",f"xyz0PUEvent{iPU}")
+  setattr(process,f"genVertex{iPU}T0Table", _genVertexT0Table.clone())
+  getattr(process,f"genVertex{iPU}T0Table").name=f"PUEvt{iPU}GenVtx"
+  getattr(process,f"genVertex{iPU}T0Table").variables = cms.PSet(
+    t0 = ExtVar(cms.InputTag("GenPUJetExtractor",f"t0PUEvent{iPU}"), "float", doc = "gen vertex t0", precision=12)
+  )
+
+process.genJetPileUpTable = _genJetTableTemp.clone()
+process.genJetPileUpTable.name = f"PileUpGenJet"
+process.genJetPileUpTable.src = cms.InputTag("GenPUJetExtractor","ak4GenJetsNoNuFromPU")
+process.genJetPileUpTable.cut = "(pt >= 5)"
+process.genJetPileUpTable.externalVariables = cms.PSet(
+  PUEventIdx = ExtVar(cms.InputTag("GenPUJetExtractor","ak4GenJetsNoNuFromPUEventIdx"), int,  doc="PUEventIdx"),
+)
+
+process.nanogenSequence = cms.Sequence(
+  process.nanoMetadata +
+  process.GenPUJetExtractor
+)
+# for iPU in range(0,nMaxPUEvent):
+#   process.nanogenSequence += getattr(process,f"genJet{iPU}Table")
+#   process.nanogenSequence += getattr(process,f"genVertex{iPU}Table")
+#   process.nanogenSequence += getattr(process,f"genVertex{iPU}T0Table")
+
+process.nanogenSequence += process.genJetPileUpTable
+
+process.nanoAOD_step = cms.Path(process.nanogenSequence)
+
+
+from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import NANOAODGENEventContent
+process.NANOAODGENoutput = cms.OutputModule("NanoAODOutputModule",
+  SelectEvents = cms.untracked.PSet(),
+  compressionAlgorithm = cms.untracked.string('LZMA'),
+  compressionLevel = cms.untracked.int32(9),
+  dataset = cms.untracked.PSet(
+      dataTier = cms.untracked.string('NANOAOD'),
+      filterName = cms.untracked.string('')
+  ),
+  # fileName = cms.untracked.string('file:NanoGen_QCD_Flat2022.root'),
+  fileName = cms.untracked.string('file:NanoGen_DYto2L.root'),
+  outputCommands = NANOAODGENEventContent.outputCommands
+)
+process.NANOAODGENoutput_step = cms.EndPath(process.nanogenSequence+process.NANOAODGENoutput)
+
+
+
+
