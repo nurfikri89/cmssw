9c267b94845fa8f204d785bca0f85688f4fe55e5
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2026-02-16 23:05:17 +0100

    Add customization functions to add AK4 CHS jets and pileup jet Id variables for AK4 Puppi jets

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -327,3 +327,212 @@ def nanoWmassGenCustomize(process):
     process.genParticleTable.variables.eta.precision=cms.string(etaPrecision)
     return process
 
+
+######################################################################################################################################################################################################
+
+PUIDVARS = cms.PSet(
+  puId_dR2Mean    = Var("?(pt>=5)?userFloat('puId_dR2Mean'):-1",float,doc="pT^2-weighted average square distance of jet constituents from the jet axis (PileUp ID BDT input variable)", precision=15),
+  puId_majW       = Var("?(pt>=5)?userFloat('puId_majW'):-1",float,doc="major axis of jet ellipsoid in eta-phi plane (PileUp ID BDT input variable)", precision=15),
+  puId_minW       = Var("?(pt>=5)?userFloat('puId_minW'):-1",float,doc="minor axis of jet ellipsoid in eta-phi plane (PileUp ID BDT input variable)", precision=15),
+  puId_frac01     = Var("?(pt>=5)?userFloat('puId_frac01'):-1",float,doc="fraction of constituents' pT contained within dR <0.1 (PileUp ID BDT input variable)", precision=15),
+  puId_frac02     = Var("?(pt>=5)?userFloat('puId_frac02'):-1",float,doc="fraction of constituents' pT contained within 0.1< dR <0.2 (PileUp ID BDT input variable)", precision=15),
+  puId_frac03     = Var("?(pt>=5)?userFloat('puId_frac03'):-1",float,doc="fraction of constituents' pT contained within 0.2< dR <0.3 (PileUp ID BDT input variable)", precision=15),
+  puId_frac04     = Var("?(pt>=5)?userFloat('puId_frac04'):-1",float,doc="fraction of constituents' pT contained within 0.3< dR <0.4 (PileUp ID BDT input variable)", precision=15),
+  puId_ptD        = Var("?(pt>=5)?userFloat('puId_ptD'):-1",float,doc="pT-weighted average pT of constituents (PileUp ID BDT input variable)", precision=15),
+  puId_beta       = Var("?(pt>=5)?userFloat('puId_beta'):-1",float,doc="fraction of pT of charged constituents associated to PV (PileUp ID BDT input variable)", precision=15),
+  puId_pull       = Var("?(pt>=5)?userFloat('puId_pull'):-1",float,doc="magnitude of pull vector (PileUp ID BDT input variable)", precision=15),
+  puId_jetR       = Var("?(pt>=5)?userFloat('puId_jetR'):-1",float,doc="fraction of jet pT carried by the leading constituent (PileUp ID BDT input variable)", precision=15),
+  puId_jetRchg    = Var("?(pt>=5)?userFloat('puId_jetRchg'):-1",float,doc="fraction of jet pT carried by the leading charged constituent (PileUp ID BDT input variable)", precision=15),
+  puId_nCharged   = Var("?(pt>=5)?userInt('puId_nCharged'):-1","int16",doc="number of charged constituents (PileUp ID BDT input variable)")
+)
+from RecoJets.JetProducers.PileupJetID_cfi import pileupJetIdCalculator
+
+def nanoAOD_addAK4CHS(process):
+
+    #
+    # Add AK4 CHS jets tasks into nanoTableTaskCommon
+    #
+    process.nanoTableTaskCommon.add(process.jetTask)
+    process.nanoTableTaskCommon.add(process.jetForMETTask)
+    process.nanoTableTaskCommon.add(process.jetTablesTask)
+    process.corrT1METJetTable.name = "CorrT1METJetCHS"
+
+    process.jetTable.name = "JetCHS"
+    process.jetTable.src = "finalJets"
+    process.jetTable.externalVariables = cms.PSet()
+    process.jetTable.variables.chMultiplicity    = Var("chargedMultiplicity()","uint8",doc="Number of charged particles in the jet")
+    process.jetTable.variables.neMultiplicity    = Var("neutralMultiplicity()","uint8",doc="Number of neutral particles in the jet")
+    process.jetTable.variables.chHEF             = Var("chargedHadronEnergyFraction()", float, doc="charged Hadron Energy Fraction", precision=15)
+    process.jetTable.variables.neHEF             = Var("neutralHadronEnergyFraction()", float, doc="neutral Hadron Energy Fraction", precision=15)
+    process.jetTable.variables.chEmEF            = Var("chargedEmEnergyFraction()", float, doc="charged Electromagnetic Energy Fraction", precision=15)
+    process.jetTable.variables.neEmEF            = Var("neutralEmEnergyFraction()", float, doc="neutral Electromagnetic Energy Fraction", precision=15)
+    process.jetTable.variables.hfHEF             = Var("HFHadronEnergyFraction()",float, doc="hadronic Energy Fraction in HF",precision=15)
+    process.jetTable.variables.hfEmEF            = Var("HFEMEnergyFraction()",float, doc="electromagnetic Energy Fraction in HF",precision=15)
+    process.jetTable.variables.muEF              = Var("muonEnergyFraction()", float, doc="muon Energy Fraction", precision=15)
+
+    del process.updatedJetsWithUserData.userFloats.leadTrackPt
+    del process.updatedJetsWithUserData.userFloats.leptonPtRelv0
+    del process.updatedJetsWithUserData.userFloats.leptonPtRelInvv0
+    del process.updatedJetsWithUserData.userFloats.leptonDeltaR
+    del process.updatedJetsWithUserData.userFloats.vtxPt
+    del process.updatedJetsWithUserData.userFloats.vtxMass
+    del process.updatedJetsWithUserData.userFloats.vtx3dL
+    del process.updatedJetsWithUserData.userFloats.vtx3deL
+    del process.updatedJetsWithUserData.userInts.vtxNtrk
+    del process.updatedJetsWithUserData.userInts.leptonPdgId
+    del process.bjetNN
+    del process.cjetNN
+
+
+    #
+    # Remove these branches for CHS
+    #
+    for varNames in process.jetTable.variables.parameterNames_():
+        if "btagDeepFlav" in varNames or "btagRobustParT" in varNames or "btagUParT" in varNames or "btagPNet" in varNames:
+            delattr(process.jetTable.variables, varNames)
+        if "UParTAK4Reg" in varNames or "PNetRegPt" in varNames:
+            delattr(process.jetTable.variables, varNames)
+        if "svIdx" in varNames or "muonIdx" in varNames or "electronIdx" in varNames:
+            delattr(process.jetTable.variables, varNames)
+        if "nElectrons" in varNames or "nMuons" in varNames or "nSVs" in varNames:
+            delattr(process.jetTable.variables, varNames)
+
+    #
+    # Calculate pileup jet ID variables
+    #
+    process.puJetIdCalculator = pileupJetIdCalculator.clone(
+        jets = "updatedJets",
+        vertexes  = "offlineSlimmedPrimaryVertices",
+        inputIsCorrected = True,
+        applyJec  = False,
+        srcConstituentWeights = ""
+    )
+    process.jetTask.add(process.puJetIdCalculator)
+
+    #
+    # Get the variables
+    #
+    process.puJetIDVar = cms.EDProducer("PileupJetIDVarProducer",
+        srcJet = cms.InputTag("updatedJets"),
+        srcPileupJetId = cms.InputTag("puJetIdCalculator")
+    )
+    process.jetTask.add(process.puJetIDVar)
+
+    #
+    # Save variables as userFloats and userInts for each jet
+    #
+    process.updatedJetsWithUserData.userFloats.puId_dR2Mean  = cms.InputTag("puJetIDVar:dR2Mean")
+    process.updatedJetsWithUserData.userFloats.puId_majW     = cms.InputTag("puJetIDVar:majW")
+    process.updatedJetsWithUserData.userFloats.puId_minW     = cms.InputTag("puJetIDVar:minW")
+    process.updatedJetsWithUserData.userFloats.puId_frac01   = cms.InputTag("puJetIDVar:frac01")
+    process.updatedJetsWithUserData.userFloats.puId_frac02   = cms.InputTag("puJetIDVar:frac02")
+    process.updatedJetsWithUserData.userFloats.puId_frac03   = cms.InputTag("puJetIDVar:frac03")
+    process.updatedJetsWithUserData.userFloats.puId_frac04   = cms.InputTag("puJetIDVar:frac04")
+    process.updatedJetsWithUserData.userFloats.puId_ptD      = cms.InputTag("puJetIDVar:ptD")
+    process.updatedJetsWithUserData.userFloats.puId_beta     = cms.InputTag("puJetIDVar:beta")
+    process.updatedJetsWithUserData.userFloats.puId_pull     = cms.InputTag("puJetIDVar:pull")
+    process.updatedJetsWithUserData.userFloats.puId_jetR     = cms.InputTag("puJetIDVar:jetR")
+    process.updatedJetsWithUserData.userFloats.puId_jetRchg  = cms.InputTag("puJetIDVar:jetRchg")
+    process.updatedJetsWithUserData.userInts.puId_nCharged   = cms.InputTag("puJetIDVar:nCharged")
+
+    #
+    # Specfiy variables in the jet table to save in NanoAOD
+    #
+    process.jetTable.variables.puId_dR2Mean  = PUIDVARS.puId_dR2Mean
+    process.jetTable.variables.puId_majW     = PUIDVARS.puId_majW
+    process.jetTable.variables.puId_minW     = PUIDVARS.puId_minW
+    process.jetTable.variables.puId_frac01   = PUIDVARS.puId_frac01
+    process.jetTable.variables.puId_frac02   = PUIDVARS.puId_frac02
+    process.jetTable.variables.puId_frac03   = PUIDVARS.puId_frac03
+    process.jetTable.variables.puId_frac04   = PUIDVARS.puId_frac04
+    process.jetTable.variables.puId_ptD      = PUIDVARS.puId_ptD
+    process.jetTable.variables.puId_beta     = PUIDVARS.puId_beta
+    process.jetTable.variables.puId_pull     = PUIDVARS.puId_pull
+    process.jetTable.variables.puId_jetR     = PUIDVARS.puId_jetR
+    process.jetTable.variables.puId_jetRchg  = PUIDVARS.puId_jetRchg
+    process.jetTable.variables.puId_nCharged = PUIDVARS.puId_nCharged
+
+    process.jetCHSMCTable = process.jetMCTable.clone(
+        name=process.jetTable.name,
+        src=process.jetTable.src,
+    )
+    process.jetCHSMCTable.variables.genJetIdx = Var("?genJetFwdRef().backRef().isNonnull() && genJetFwdRef().backRef().pt() > 10.?genJetFwdRef().backRef().key():-1", "int16", doc="index of matched gen jet")
+    process.jeCHSMCTask = cms.Task(process.jetCHSMCTable)
+    process.jetMCTask = process.jetMCTask.copyAndAdd(process.jeCHSMCTask)
+
+    return process
+
+from PhysicsTools.PatAlgos.tools.jetTools import setupPuppiForPackedPF
+def nanoAOD_AddPileUpJetIDVarsAK4Puppi(process):
+
+    nanoAOD_rePuppi_switch = cms.PSet(
+        useExistingWeights=cms.bool(False),
+    )
+    # recompute Puppi weights
+    (run2_nanoAOD_106Xv2 | run3_nanoAOD_pre142X | nanoAOD_rePuppi).toModify(
+        nanoAOD_rePuppi_switch, useExistingWeights=False
+    )
+
+    if not(hasattr(process, "packedpuppi")):
+        packedpuppi, packedpuppiNoLep = setupPuppiForPackedPF(process,
+            useExistingWeights=nanoAOD_rePuppi_switch.useExistingWeights.value(),
+        )
+
+    #
+    # Calculate pileup jet ID variables
+    #
+    process.puJetIdCalculatorPuppi = pileupJetIdCalculator.clone(
+        jets = "updatedJetsPuppi",
+        vertexes  = "offlineSlimmedPrimaryVertices",
+        inputIsCorrected = True,
+        applyJec  = False,
+        srcConstituentWeights = "packedpuppi"
+    )
+    process.jetPuppiTask.add(process.puJetIdCalculatorPuppi)
+
+    #
+    # Get the variables
+    #
+    process.puJetIDVarPuppi = cms.EDProducer("PileupJetIDVarProducer",
+        srcJet = cms.InputTag("updatedJetsPuppi"),
+        srcPileupJetId = cms.InputTag("puJetIdCalculatorPuppi")
+    )
+    process.jetPuppiTask.add(process.puJetIDVarPuppi)
+
+    #
+    # Save variables as userFloats and userInts for each jet
+    #
+    process.updatedJetsPuppiWithUserData.userFloats.puId_dR2Mean  = cms.InputTag("puJetIDVarPuppi:dR2Mean")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_majW     = cms.InputTag("puJetIDVarPuppi:majW")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_minW     = cms.InputTag("puJetIDVarPuppi:minW")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_frac01   = cms.InputTag("puJetIDVarPuppi:frac01")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_frac02   = cms.InputTag("puJetIDVarPuppi:frac02")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_frac03   = cms.InputTag("puJetIDVarPuppi:frac03")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_frac04   = cms.InputTag("puJetIDVarPuppi:frac04")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_ptD      = cms.InputTag("puJetIDVarPuppi:ptD")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_beta     = cms.InputTag("puJetIDVarPuppi:beta")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_pull     = cms.InputTag("puJetIDVarPuppi:pull")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_jetR     = cms.InputTag("puJetIDVarPuppi:jetR")
+    process.updatedJetsPuppiWithUserData.userFloats.puId_jetRchg  = cms.InputTag("puJetIDVarPuppi:jetRchg")
+    process.updatedJetsPuppiWithUserData.userInts.puId_nCharged   = cms.InputTag("puJetIDVarPuppi:nCharged")
+
+    #
+    # Specfiy variables in the jet table to save in NanoAOD
+    #
+    process.jetPuppiTable.variables.puId_dR2Mean  = PUIDVARS.puId_dR2Mean
+    process.jetPuppiTable.variables.puId_majW     = PUIDVARS.puId_majW
+    process.jetPuppiTable.variables.puId_minW     = PUIDVARS.puId_minW
+    process.jetPuppiTable.variables.puId_frac01   = PUIDVARS.puId_frac01
+    process.jetPuppiTable.variables.puId_frac02   = PUIDVARS.puId_frac02
+    process.jetPuppiTable.variables.puId_frac03   = PUIDVARS.puId_frac03
+    process.jetPuppiTable.variables.puId_frac04   = PUIDVARS.puId_frac04
+    process.jetPuppiTable.variables.puId_ptD      = PUIDVARS.puId_ptD
+    process.jetPuppiTable.variables.puId_beta     = PUIDVARS.puId_beta
+    process.jetPuppiTable.variables.puId_pull     = PUIDVARS.puId_pull
+    process.jetPuppiTable.variables.puId_jetR     = PUIDVARS.puId_jetR
+    process.jetPuppiTable.variables.puId_jetRchg  = PUIDVARS.puId_jetRchg
+    process.jetPuppiTable.variables.puId_nCharged = PUIDVARS.puId_nCharged
+
+    return process
+
+######################################################################################################################################################################################################
